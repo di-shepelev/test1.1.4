906b337c4582ca5073bf321206e292a503d19041
di-shepelev <di.shplv@gmail.com>
2023-04-29 16:36:27 +0300

    1.1.4
    1.1.4

---
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,24 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        UserService UserService = new UserServiceImpl();
+
+        UserService.createUsersTable();
+
+        UserService.saveUser("Name1", "LastName1", (byte) 20);
+        UserService.saveUser("Name2", "LastName2", (byte) 25);
+        UserService.saveUser("Name3", "LastName3", (byte) 31);
+        UserService.saveUser("Name4", "LastName4", (byte) 38);
+
+        UserService.getAllUsers();
+        UserService.cleanUsersTable();
+        UserService.dropUsersTable();
+
+    }
+}
